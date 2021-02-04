e1d648a5d814eb946dffc1a35b81fb00084bda25
Alexey Balabukha <leha.alexey1909@gmail.com>
2021-02-04 17:04:37 +0200

    fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -22,7 +22,7 @@ const loginSchema = {
 const login = (req, res) => {
   const data = req.body;
   const { email, password } = req.body;
-  return Joi.validate(data, loginSchema, async (err, value) => {
+  Joi.validate(data, loginSchema, async (err, value) => {
     if (err) {
       const str = err.details[0].path[0]
       res.status(500).json({ message: `${validateMessage(str)} is required` });
@@ -32,23 +32,24 @@ const login = (req, res) => {
       res.status(500).json({ message: "Email must be valid" });
       return null
     }
+
     await User.find({ email }, async (err, user) => {
-      const match = await bCrypt.compare(password, user[0]?.password);
       if (user.length === 0) {
-        res
-          .status(401)
-          .json({ message: "User does not exist!" });
-      } else if (match) {
-        res.status(200).json({
+        return res
+          .status(422)
+          .send({ message: "User does not exist!" });
+      } 
+      let test = bCrypt.compare(password, user[0].password)
+      if (test) {
+        return res.status(200).send({
           token: createToken({ id: user[0]._id }),
           id: user[0]._id,
           message: "Authenticated successfully"
         });
-      } else {
-        res
-          .status(401)
-          .json({ message: "Incorrect password" });
-      }
+      } 
+      return res
+        .status(422)
+        .send({ message: "Incorrect password" });
     })
   });
 };
@@ -56,7 +57,7 @@ const login = (req, res) => {
 const register = (req, res) => {
   const data = req.body;
   const { name, email, password } = req.body;
-  return Joi.validate(data, registerSchema, async (err) => {
+  Joi.validate(data, registerSchema, async (err) => {
     
     if (err) {
       const str = err.details[0].path[0]
@@ -72,7 +73,7 @@ const register = (req, res) => {
       res.status(500).json({ message: `Password no less than symbols` });
     }
     await User.find({ email }, (err, user) => {
-      if (user?.length !== 0) {
+      if (user.length !== 0) {
         return res
           .status(409)
           .json({ message: "User already exsist"});
